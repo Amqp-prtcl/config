b67663a1bbbae3f0816f25b7c7956345537deeea
Amqp-prtcl <Amqp.protocol@gmail.com>
2022-08-31 22:42:05 +0200

    sync

--- key.go
+++ key.go
@@ -54,18 +54,33 @@ func (k Key[T]) Get(c *Config) T {
 	return ret
 }
 
-/*func (k Key[T]) GetErr(c *Config) (T, error) {
+func (k Key[T]) GetErr(c *Config) (T, error) {
 	var ret T
 	v, ok := c.Get(string(k))
 	if !ok {
 		return ret, ErrKeyNotFound
+	}
+	switch v := v.(type) {
+	case T:
+		return v, nil
+	case bool:
+		if reflect.ValueOf(ret).Kind() == reflect.String {
+			return interface{}(strconv.FormatBool(v)).(T), nil
+		}
+	case string:
+		if reflect.ValueOf(ret).Kind() == reflect.Bool {
+			b, err := strconv.ParseBool(v)
+			if err == nil {
+				return interface{}(b).(T), nil
+			}
+		}
 	}
 	rv := reflect.ValueOf(v)
 	if rv.CanConvert(reflect.TypeOf(ret)) {
 		return rv.Convert(reflect.TypeOf(ret)).Interface().(T), nil
 	}
 	return ret, fmt.Errorf("config file Get: failed to cast value (wanted type: %T but got type: %T)", ret, v)
-}*/
+}
 
 func (k Key[T]) Put(c *Config, v T) {
 	c.Put(string(k), v)
@@ -80,7 +95,7 @@ func (k TimeKey) Get(c *Config) time.Time {
 	return t
 }
 
-/*func (k TimeKey) GetErr(c *Config) (time.Time, error) {
+func (k TimeKey) GetErr(c *Config) (time.Time, error) {
 	var t time.Time
 	var ck = Key[string](k)
 	str, err := ck.GetErr(c)
@@ -89,7 +104,7 @@ func (k TimeKey) Get(c *Config) time.Time {
 	}
 	err = t.UnmarshalText([]byte(str))
 	return t, err
-}*/
+}
 
 func (k TimeKey) Put(c *Config, v time.Time) {
 	c.Put(string(k), v)
